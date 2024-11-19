bf17576f98983df00f9cd58022acf144ea1c0742
Jack <jscheiber@wisc.edu>
2024-11-18 20:41:36 -0600

    init commit for VCS, already added skeleton and basic funtionality for student/teacher data and class data

---
+++ app/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("com.google.devtools.ksp")
+}
+
+android {
+    namespace = "com.cs407.user_data"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.cs407.user_data"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.core.ktx)
+    implementation(libs.androidx.room.paging)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.paging.runtime.ktx)
+    implementation(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ app/src/main/java/com/cs407/user_data/ClassDatabase.kt
@@ -0,0 +1,139 @@
+package com.cs407.user_data
+
+import android.content.Context
+import androidx.paging.PagingSource
+import androidx.room.ColumnInfo
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.Insert
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.Transaction
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+import androidx.room.Upsert
+import java.util.Date
+
+@Entity
+data class Class(
+    @PrimaryKey val classId: String,
+    val className: String,
+)
+
+@Entity
+data class Student(
+    @PrimaryKey val studentId: String,
+    val name: String,
+    val email: String
+)
+
+@Entity
+data class Teacher(
+    @PrimaryKey val teacherId: String,
+    val name: String,
+    val email: String
+)
+
+@Entity(
+    primaryKeys = ["classId", "studentId"],
+    foreignKeys = [
+        ForeignKey(entity = Class::class, parentColumns = ["classId"], childColumns = ["classId"]),
+        ForeignKey(entity = Student::class, parentColumns = ["studentId"], childColumns = ["studentId"])
+    ]
+)
+data class StudentClassRelation(
+    val classId: String,
+    val studentId: String
+)
+
+@Entity(
+    primaryKeys = ["classId", "teacherId"],
+    foreignKeys = [
+        ForeignKey(entity = Class::class, parentColumns = ["classId"], childColumns = ["classId"]),
+        ForeignKey(entity = Teacher::class, parentColumns = ["teacherId"], childColumns = ["teacherId"])
+    ]
+)
+data class TeacherClassRelation(
+    val classId: String,
+    val teacherId: String
+)
+
+@Dao
+interface ClassDao {
+    @Insert(entity = Class::class)
+    suspend fun insertClass(_class: Class)
+
+    @Query("SELECT * FROM Class WHERE classId = :classId")
+    suspend fun getClassById(classId: String): Class?
+}
+
+@Dao
+interface StudentDao {
+    @Insert(entity = Student::class)
+    suspend fun insertStudent(student: Student)
+
+    @Query("SELECT * FROM Student WHERE studentId = :studentId")
+    suspend fun getStudentById(studentId: String): Student?
+
+    @Query(
+        """SELECT * FROM Student, Class, StudentClassRelation
+            WHERE Class.classId = :classId
+                AND StudentClassRelation.studentId = Student.studentId
+                AND Class.classId = StudentClassRelation.classId
+                """
+    )
+    suspend fun getStudentsInClass(classId: Int): List<Student>
+}
+
+@Dao
+interface TeacherDao {
+    @Insert(entity = Teacher::class)
+    suspend fun insertTeacher(teacher: Teacher)
+
+    @Query("SELECT * FROM Teacher WHERE teacherId = :teacherId")
+    suspend fun getTeacherById(teacherId: String): Teacher?
+
+    @Query(
+        """SELECT * FROM Teacher, Class, TeacherClassRelation
+            WHERE Class.classId = :classId
+                AND TeacherClassRelation.teacherId = Teacher.teacherId
+                AND Class.classId = TeacherClassRelation.classId
+                """
+    )
+    suspend fun getTeachersInClass(classId: Int): List<Teacher>
+}
+
+@Dao
+interface DeleteDao {
+    @Query("DELETE FROM student WHERE studentId = :studentId")
+    suspend fun deleteStudent(studentId: Int)
+}
+
+@Database(
+    entities = [Class::class, Student::class, Teacher::class, StudentClassRelation::class,
+                                                                     TeacherClassRelation::class],
+    version = 1
+)
+abstract class ClassDatabase : RoomDatabase() {
+    abstract fun classDao(): ClassDao
+    abstract fun studentDao(): StudentDao
+    abstract fun teacherDao(): TeacherDao
+
+    companion object {
+        @Volatile private var instance: ClassDatabase? = null
+
+        fun getInstance(context: Context): ClassDatabase =
+            instance ?: synchronized(this) {
+                instance ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    ClassDatabase::class.java,
+                    "Class Database"
+                ).build()
+            }
+    }
+}
